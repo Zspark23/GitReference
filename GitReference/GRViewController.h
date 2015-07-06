b0beaea1ce04ee86a87bb8bc40e87d3c7d29e875
Zack Spicer <spicer.zack8@gmail.com>
2015-07-06 17:28:20 -0600

    Added ViewController

---
+++ GitReference/GRViewController.h
@@ -0,0 +1,13 @@
+//
+//  GRViewController.h
+//  GitReference
+//
+//  Created by Zack Spicer on 7/6/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRViewController : UIViewController
+
+@end
